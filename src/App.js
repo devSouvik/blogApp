b7c99d23b0d915a24b5930f74bdb5aea70b507e5
devSouvik <souvikguria98@gmail.com>
2021-12-30 11:03:15 +0530

    routes added and setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/createpost">Create Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/login" exact element={<Login />}></Route>
+        <Route path="/createpost" exact element={<CreatePost />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCox9mkoC02lqHM0RN3CbAVZyInBGdKlC8",
+  authDomain: "blogproject-2ba92.firebaseapp.com",
+  projectId: "blogproject-2ba92",
+  storageBucket: "blogproject-2ba92.appspot.com",
+  messagingSenderId: "1002664811691",
+  appId: "1:1002664811691:web:c003e8cadb7715d8002bee",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
