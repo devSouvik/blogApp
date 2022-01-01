87e4e85a35924521cc89fb381284dbcf4d235d5f
devSouvik <souvikguria98@gmail.com>
2022-01-01 09:38:38 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,49 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreatePost from "./pages/CreatePost";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signuUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/createpost">Create Post</Link>
+        {!isAuth ? (
+          <Link to="/login">Login</Link>
+        ) : (
+          <>
+            {/* <button onClick={signuUserOut}>Sign Out</button> */}
+            <span onClick={signuUserOut} className="signOut">
+              Sign out
+            </span>
+            <Link to="/createpost">Create Post</Link>
+          </>
+        )}
       </nav>
       <Routes>
-        <Route path="/" exact element={<Home />}></Route>
-        <Route path="/login" exact element={<Login />}></Route>
-        <Route path="/createpost" exact element={<CreatePost />}></Route>
+        <Route path="/" exact element={<Home isAuth={isAuth} />}></Route>
+        <Route
+          path="/login"
+          exact
+          element={<Login setIsAuth={setIsAuth} />}
+        ></Route>
+        <Route
+          path="/createpost"
+          exact
+          element={<CreatePost isAuth={isAuth} />}
+        ></Route>
       </Routes>
     </Router>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,25 @@
-function Login() {
-  return <div>Login</div>;
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+  return (
+    <div className="loginPage">
+      <p>sign in with google to continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        sign in with google
+      </button>
+    </div>
+  );
 }
 
 export default Login;
